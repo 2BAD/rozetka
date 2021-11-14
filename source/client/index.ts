b2229ef27ef317baf9123e768dacf67fccef66e6
Jason Hyde <github@2bad.me>
2021-11-14 23:56:55 +0200

    [ts] create catalog object by extending rozetka

--- source/client/index.ts
+++ source/client/index.ts
@@ -1,6 +1,6 @@
 import got from 'got'
-import { logRequest, logResponse } from './hooks/debug'
-import { updateSearchParams } from './hooks/searchParams'
+import { logRequest, logResponse } from './hooks/debug.js'
+import { updateSearchParams } from './hooks/searchParams.js'
 
 export const rozetka = got.extend({
   headers: {
@@ -15,5 +15,14 @@ export const rozetka = got.extend({
     ['front-type', 'xl'],
     ['country', 'UA'],
     ['lang', 'ru']
+  ])
+})
+
+export const catalog = rozetka.extend({
+  prefixUrl: 'https://xl-catalog-api.rozetka.com.ua/',
+  searchParams: new URLSearchParams([
+    ['with_groups', '1'],
+    ['with_docket', '1'],
+    ['goods_group_href', '1']
   ])
 })
