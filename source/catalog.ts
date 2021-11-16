147d77b56e310b52a333c49d198a336794479f57
Jason Hyde <github@2bad.me>
2021-11-16 18:32:40 +0200

    [ts] implement catalog.details()

--- source/catalog.ts
+++ source/catalog.ts
@@ -3,6 +3,13 @@ import { catalog, common } from './client/index'
 
 export const categories = () => common.get('v2/fat-menu/full')
 
+export const details = (categoryId: number) => {
+  const params: Params = {
+    searchParams: new URLSearchParams([['id', categoryId.toString()]])
+  }
+  return catalog.get('v4/categories/get', params)
+}
+
 // @todo should paginate automatically
 export const ids = (categoryId: number) => {
   const params: Params = {
