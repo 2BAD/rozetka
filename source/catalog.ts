d3163e69a7338479a213cfd4deb78ca5f28e565f
Jason Hyde <github@2bad.me>
2021-11-16 18:12:20 +0200

    [ts] implement catalog.ids()

--- source/catalog.ts
+++ source/catalog.ts
@@ -1,3 +1,11 @@
-import { common } from './client/index'
+import type { OptionsOfTextResponseBody as Params } from 'got'
+import { catalog, common } from './client/index'
 
 export const categories = () => common.get('v2/fat-menu/full')
+
+export const ids = (categoryId: number) => {
+  const params: Params = {
+    searchParams: new URLSearchParams([['category_id', categoryId.toString()]])
+  }
+  return catalog.get('v4/goods/get', params)
+}
