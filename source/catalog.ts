f32beb2d7a15bf16caeaf67f232961dd026ffea0
Jason Hyde <github@2bad.me>
2021-11-22 04:23:07 +0200

    [ts] use buildParams helper function to build query string

--- source/catalog.ts
+++ source/catalog.ts
@@ -1,19 +1,15 @@
-import type { OptionsOfTextResponseBody as Params } from 'got'
 import { catalog, common } from './client/index'
+import { buildParams } from './client/utils/params'
 
 export const categories = () => common.get('v2/fat-menu/full')
 
-export const details = (categoryId: number) => {
-  const params: Params = {
-    searchParams: new URLSearchParams([['id', categoryId.toString()]])
-  }
-  return catalog.get('v4/categories/get', params)
-}
+export const details = (categoryId: number) =>
+  catalog.get('v4/categories/get', buildParams([['id', categoryId.toString()]]))
 
 // @todo should paginate automatically
 export const ids = (categoryId: number) => {
-  const params: Params = {
-    searchParams: new URLSearchParams([['category_id', categoryId.toString()]])
-  }
-  return catalog.get('v4/goods/get', params)
+  return catalog.get(
+    'v4/goods/get',
+    buildParams([['category_id', categoryId.toString()]])
+  )
 }
