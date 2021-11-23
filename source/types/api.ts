f7cfa6dc0839e2bf12356e922e3b544d55d16eb3
Jason Hyde <github@2bad.me>
2021-11-23 06:16:36 +0200

    [ts] refine api response check to handle nullish data

--- source/types/api.ts
+++ source/types/api.ts
@@ -1,6 +1,10 @@
 import { z } from 'zod'
 
-export const Response = z.object({
-  data: z.union([z.array(z.unknown()), z.unknown()]),
-  success: z.boolean().optional()
-})
+export const Response = z
+  .object({
+    data: z.union([z.array(z.unknown()), z.unknown()]),
+    success: z.boolean().optional()
+  })
+  .refine((v) => v.data !== null, {
+    message: `Response data property is null`
+  })
