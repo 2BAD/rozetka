f68677a382a70e6b273382df00227cfe07fc11eb
Jason Hyde <github@2bad.me>
2021-12-07 19:26:57 +0200

    [ts] fetch all ids using pagination and return single array

--- source/catalog.ts
+++ source/catalog.ts
@@ -10,10 +10,7 @@ export const categories = (): Promise<Catalog> =>
 
 export const children = (categoryId: number): Promise<Children> =>
   catalog
-    .get(
-      'v4/categories/getChildren',
-      buildParams([['category_id', categoryId.toString()]])
-    )
+    .get('v4/categories/getChildren', buildParams([['category_id', categoryId.toString()]]))
     .then((r) => Children.parse(r))
 
 export const details = (categoryId: number): Promise<Details> =>
@@ -21,8 +18,34 @@ export const details = (categoryId: number): Promise<Details> =>
     .get('v4/categories/get', buildParams([['id', categoryId.toString()]]))
     .then((r) => Details.parse(r))
 
-// @todo should paginate automatically
-export const ids = (categoryId: number): Promise<Ids> =>
-  catalog
-    .get('v4/goods/get', buildParams([['category_id', categoryId.toString()]]))
-    .then((r) => Ids.parse(r))
+export const ids = (categoryId: number): Promise<number[]> =>
+  catalog.paginate.all('v4/goods/get', {
+    searchParams: new URLSearchParams([['category_id', categoryId.toString()]]),
+    pagination: {
+      transform: (response) => {
+        const data = Ids.parse(response.body)
+        return data.ids
+      },
+      paginate: ({ response }) => {
+        // Get the last retrieved page number
+        const { searchParams } = response.request.options
+        const previousPage =
+          searchParams instanceof URLSearchParams ? Number(searchParams.get('page') ?? 1) : 1
+
+        // If there are no more data, finish
+        const data = Ids.parse(response.body)
+        if (data.total_pages === previousPage) {
+          return false
+        }
+
+        // Otherwise, update the page number by one and continue
+        return {
+          searchParams: {
+            page: previousPage + 1
+          }
+        }
+      },
+      // Wait 1s before making another request to prevent API rate limiting.
+      backoff: 1000
+    }
+  })
