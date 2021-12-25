67b15099a85be3821915d2e90997aec55da03f44
Jason Hyde <github@2bad.me>
2021-12-25 02:58:44 +0200

    [test] snapshot tests for catalog endpoints

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Unofficial REST API client for rozetka.ua
 
-Tests
+### Tests
+
 Running tests requires `--experimental-vm-modules` flag
 example: `NODE_OPTIONS=--experimental-vm-modules npm run test`
+
+### Note
+
+Sometimes catalog categories may have wrong `category_id` and `top_category_id` (e.g. 80025), first is mitigated by extracting correct `category_id` from the link, second one should not be relied upon and replaced with warning message `NOT_RELIABLE_SEE_README`.
---
+++ test/catalog.ts
@@ -0,0 +1,26 @@
+import * as catalog from '../source/catalog'
+
+describe('catalog', () => {
+  const CATEGORY_ID = 4649166
+
+  it('categories should match snapshot', async () => {
+    expect.assertions(1)
+    const categories = await catalog.categories()
+    expect(categories).toMatchSnapshot()
+  })
+  it('children should match snapshot', async () => {
+    expect.assertions(1)
+    const children = await catalog.children(CATEGORY_ID)
+    expect(children).toMatchSnapshot()
+  })
+  it('details should match snapshot', async () => {
+    expect.assertions(1)
+    const details = await catalog.details(CATEGORY_ID)
+    expect(details).toMatchSnapshot()
+  })
+  it('ids should match snapshot', async () => {
+    expect.assertions(1)
+    const ids = await catalog.ids(CATEGORY_ID)
+    expect(ids).toMatchSnapshot()
+  }, 20000)
+})
