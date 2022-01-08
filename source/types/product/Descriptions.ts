68e488c233611806dac249e6f1336abb0875b7a2
Jason Hyde <github@2bad.me>
2022-01-09 00:47:15 +0200

    [docs] uniform naming for complex object types

--- source/types/product/Descriptions.ts
+++ source/types/product/Descriptions.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod'
 
-export const Description = z.object({
+export const Descriptions = z.object({
   text: z.string().nullable(),
   html: z.string().nullable(),
   isRichNoLimit: z.boolean(),
   rich: z.string().nullable()
 })
 // eslint-disable-next-line @typescript-eslint/no-redeclare
-export type Description = z.infer<typeof Description>
+export type Descriptions = z.infer<typeof Descriptions>
