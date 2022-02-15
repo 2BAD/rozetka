276f3e6a89a35ef54869e504b687ce7afe6ace8e
Jason Hyde <github@2bad.me>
2022-02-16 01:22:18 +0200

    [test] add case for empty response

--- source/products/characteristics/test.unit.ts
+++ source/products/characteristics/test.unit.ts
@@ -1,15 +1,23 @@
 import { server } from '../../utils/server'
-import { PRODUCT_ID } from '../fixtures'
+import { PRODUCT_ID, WRONG_PRODUCT_ID } from '../fixtures'
 import { get } from './index'
 
 describe('/products/characteristics', () => {
   describe('GET', () => {
     it('When asked for an existing product \n\t Then should be able to retrieve and parse its characteristics', async () => {
       expect.assertions(1)
-      const { done } = await server(import.meta.url, 'stub.json')
+      const { done } = await server(import.meta.url, 'good.json')
 
       const r = await get(PRODUCT_ID)
       expect(r.data).toMatchSnapshot()
+      done()
+    })
+    it('When asked for an non-existing product \n\t Then should return an Error object', async () => {
+      expect.assertions(1)
+      const { done } = await server(import.meta.url, 'empty.json')
+
+      const r = await get(WRONG_PRODUCT_ID)
+      expect(r.error).toMatchSnapshot()
       done()
     })
   })
