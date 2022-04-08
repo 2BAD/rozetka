ec36c75dccd2b49636336706d155c5d8600b5f92
Jason Hyde <github@2bad.me>
2022-04-08 17:21:53 +0100

    [ts] build params from records

--- source/client/utils/params.ts
+++ source/client/utils/params.ts
@@ -3,6 +3,6 @@ import { OptionsOfTextResponseBody as Params } from 'got'
 /**
  * Create Options object with searchParams populated
  */
-export const params = (...params: string[][]): Params => ({
+export const params = (params: Record<string, string>): Params => ({
   searchParams: new URLSearchParams(params)
 })
--- source/client/utils/test.unit.ts
+++ source/client/utils/test.unit.ts
@@ -4,17 +4,17 @@ describe('parameter builder', () => {
   it('should return Options object with searchParam set to URLSearchParams object instance', () => {
     expect.assertions(3)
 
-    const p = params(['goodsId', '100'])
+    const p = params({ goodsId: '100' })
 
     expect(p).toHaveProperty('searchParams')
     expect(p.searchParams).toBeInstanceOf(URLSearchParams)
     expect(p).toMatchSnapshot()
   })
 
-  it('should accept an array of arrays', () => {
+  it('should accept an object with records', () => {
     expect.assertions(2)
 
-    const p = params(['goodsId', '100'], ['lang', 'ru'])
+    const p = params({ goodsId: '100', lang: 'ru' })
 
     expect(p.searchParams?.toString()).toBe('goodsId=100&lang=ru')
     expect(p).toMatchSnapshot()
